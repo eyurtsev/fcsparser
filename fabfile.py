4c9dc4939b1d4755832d7bee4f443d1c147f3e0a
Eugene Yurtsev <eyurtsev@gmail.com>
2015-09-13 18:20:45 -0400

    Updating fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,46 +9,24 @@ from contextlib import contextmanager
 from fabric.api import local, lcd, abort
 from fabric.decorators import task
 
-DL_DIR = "doc/source/_static/downloads"
-
 BUILD_DIRS = (
     "dist",
     "doc/build",
     "build",
-    "FlowCytometryTools.egg-info",
+    "fcsparser.egg-info",
 )
 
 SDIST_RST_FILES = (
     "README.rst",
-    #"HISTORY.rst",
 )
 
 SDIST_TXT_FILES = [os.path.splitext(x)[0] + ".txt" for x in SDIST_RST_FILES]
 
-
-###############################################################################
-# Misc.
-###############################################################################
 @task
 def clean():
     """Clean build files."""
     for build_dir in list(BUILD_DIRS):
         local("rm -rf %s" % build_dir)
-
-@task
-def html():
-    """Make html files."""
-    with lcd("doc"):
-        local("make html")
-
-@task
-def serve(port="8000"):
-    """
-    Serve website from localhost.
-    @param port  Port to run server on.
-    """
-    with lcd("doc/build/html"):
-        local("python -m SimpleHTTPServer %s" % port)
 
 
 ###############################################################################
@@ -93,43 +71,3 @@ def pypi_upload():
     """Upload package."""
     with _dist_wrapper():
         local("python setup.py sdist upload", capture=False)
-
-
-###############################################################################
-# Downloads
-###############################################################################
-#def get_rev(tag=True):
-    #"""Get build revision.
-#
-    #@param tag  Use git tag instead of hash?
-    #"""
-    #rev_cmd = "git describe --always --tag" if tag in (True, "True") else \
-              #"git rev-parse HEAD"
-    #return local(rev_cmd, capture=True).strip()
-#
-#
-#@task
-#def zip_bundle(tag=True):
-    #"""Create zip file upload bundles.
-#
-    #@param tag  Use git tag instead of hash?
-    #"""
-    ##rev = get_rev(tag)
-    #rev = __version__
-#
-    #print("Cleaning old build files.")
-    #clean()
-#
-    #local("mkdir -p build")
-#
-    #print("Bundling new files.")
-    #with lcd("sphinx_bootstrap_theme/bootstrap"):
-        #local("zip -r ../../build/bootstrap.zip .")
-#
-    #dest = os.path.abspath(os.path.join(DL_DIR, rev))
-    #with lcd("build"):
-        #local("mkdir -p %s" % dest)
-        #local("cp bootstrap.zip %s" % dest)
-#
-        #print("Verifying contents.")
-        #local("unzip -l bootstrap.zip")
