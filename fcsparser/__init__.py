bbb1d6c255f0eb936bb11fa61da918f0cf3b9dac
Eugene Yurtsev <eyurtsev@gmail.com>
2015-09-13 19:19:15 -0400

    python 3+ compatibility and adding test data path

--- fcsparser/__init__.py
+++ fcsparser/__init__.py
@@ -1,3 +1,8 @@
-from _version import version as __version__
+import os
 
-from api import parse
+from fcsparser._version import version as __version__
+from fcsparser.api import parse
+
+test_sample_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+    'tests', 'data', 'FlowCytometers', 'HTS_BD_LSR-II', 'HTS_BD_LSR_II_Mixed_Specimen_001_D6_D06.fcs')
+
