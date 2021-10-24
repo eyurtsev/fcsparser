eccbc355b7e6e717c29b7449dc18dae887fd8107
Eugene Yurtsev <eyurtsev@gmail.com>
2021-10-24 13:19:28 -0400

    update

--- fcsparser/__init__.py
+++ fcsparser/__init__.py
@@ -1,8 +1,10 @@
 import os
 
-from fcsparser._version import version as __version__
 from fcsparser.api import parse
 
 test_sample_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
     'tests', 'data', 'FlowCytometers', 'HTS_BD_LSR-II', 'HTS_BD_LSR_II_Mixed_Specimen_001_D6_D06.fcs')
 
+
+from . import _version
+__version__ = _version.get_versions()['version']
