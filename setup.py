92c3abf384c061cce3eddf8bb352be510897e113
Eugene Yurtsev <eyurtsev@gmail.com>
2021-10-24 13:22:10 -0400

    update (#39)

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
-import re
 import fnmatch
 import os
 
 from setuptools import setup, find_packages
 
-
-def get_package_version(path):
-    '''Extracts the version'''
-    with open(VERSION_FILE, "rt") as f:
-        verstrline = f.read()
-
-    VERSION = r"^version = ['\"]([^'\"]*)['\"]"
-    results = re.search(VERSION, verstrline, re.M)
-
-    if results:
-        version = results.group(1)
-    else:
-        raise RuntimeError("Unable to find version string in {}.".format(path))
-
-    return version
+import versioneer
 
 
 def get_fcs_files():
@@ -29,22 +14,18 @@ def get_fcs_files():
     return matches
 
 
-VERSION_FILE = "fcsparser/_version.py"
-
-version = get_package_version(VERSION_FILE)
-
 with open('README.rst', 'r') as f:
     README_content = f.read()
 
 setup(
     name='fcsparser',
     packages=find_packages(),
-    version=version,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='A python package for reading raw fcs files',
     author='Eugene Yurtsev',
     author_email='eyurtsev@gmail.com',
     url='https://github.com/eyurtsev/fcsparser',
-    download_url='https://github.com/eyurtsev/fcsparser/archive/v{0}.zip'.format(version),
     keywords=['flow cytometry', 'data analysis', 'cytometry', 'parser', 'data'],
     license='MIT',
 
