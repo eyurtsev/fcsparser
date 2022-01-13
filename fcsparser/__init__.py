5a4caabf8114babf4bab1a963050a598d2c4b13c
Brian Teague <bpteague@gmail.com>
2022-01-12 21:28:10 -0600

    Make __init__.py use a relative import

--- fcsparser/__init__.py
+++ fcsparser/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from fcsparser.api import parse
+from .api import parse
 
 test_sample_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
     'tests', 'data', 'FlowCytometers', 'HTS_BD_LSR-II', 'HTS_BD_LSR_II_Mixed_Specimen_001_D6_D06.fcs')
